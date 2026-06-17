c41aa6566870dfceccecf57f295f9a69d26796f3
BadPlays <Haydanfrench@hotmail.com>
2026-06-17 17:20:53 +0100

    Removed Conflict Issues

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,11 +16,7 @@ public class PlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
-<<<<<<< Updated upstream
-        
-=======
         tripLevel = 0;
->>>>>>> Stashed changes
     }
 
     private void OnEnable()
